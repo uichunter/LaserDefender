b0c0e365a0abe343bce0d2c042be653e6d001b46
uichunter <pankettingpanda@gmail.com>
2016-03-04 10:00:53 -0600

    Add SFX to game.
    Projectile SFX.
    Enemy crash SFX.

---
+++ Assets/Scripts/BottonController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottonController : MonoBehaviour {
+	public AudioClip buttonPressSound;
+
+	//private AudioSource source;
+
+	void OnMouseEnter ()
+	{
+		AudioSource.PlayClipAtPoint(buttonPressSound,transform.position);
+		Debug.Log("Mouse over the button!");
+	}
+}
